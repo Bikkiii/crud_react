21ce13d9445f0ff126817c6377565c4233a38f77
Bikkiii <bikashdhami15111@gmail.com>
2026-02-16 16:00:52 +0545

    Added edit feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import Items from "./components/Items";
 import { groceryItems } from "./data/groceryItems";
 import "./App.css";
 
 const App = () => {
+  const [items, setItems] = useState(groceryItems);
+
+  const editCompleted = (itemId) => {
+    const newItems = items.map((item) => {
+      if (item.id === itemId) {
+        return { ...item, completed: !item.completed };
+      }
+      return item;
+    });
+    setItems(newItems);
+  };
+
   return (
     <section className="section-center">
-      <Items items={groceryItems} />
+      <Items items={items} editCompleted={editCompleted} />
     </section>
   );
 };
